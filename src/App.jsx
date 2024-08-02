2eff58111e12958cf0db24998b3e3ed52b2929c7
Codedys <oberegeraldjackson@gmail.com>
2024-08-02 17:53:28 +0300

    top header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <div className="container">
+      <nav>
+        <div className="userinfo">
+          <img src="./profile.jpeg" alt="" />
+          <span>Gerald Jackson</span>
+          <img className="icon" src="./light.png" alt="" />
+        </div>
+        <div className="socials">
+          <img src="./github.png" alt="" />
+          <img src="linkedin.png" alt="" />
+          <img src="twitter.png" alt="" />
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default App
+export default App;
