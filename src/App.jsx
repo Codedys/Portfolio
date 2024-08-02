e635144822e7dc413bff5cf38adc6e9fca985cac
Codedys <oberegeraldjackson@gmail.com>
2024-08-03 00:19:44 +0300

    responsive sidebar added

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,41 @@ const App = () => {
           <img src="twitter.png" alt="" />
         </div>
       </nav>
+
+
+      
+      <main>
+        <div className="sidebar">
+          <div className="route-elements">
+            <img src="home.png" alt="" />
+            <span>Home</span>
+          </div>
+          <div className="route-elements">
+            <img src="about.png" alt="" />
+            <span>About</span>
+          </div>
+          <div className="route-elements">
+            <img src="projects.png" alt="" />
+            <span>Projects</span>
+          </div>
+          <div className="route-elements">
+            <img src="blog.png" alt="" />
+            <span>Blog</span>
+          </div>
+          <div className="route-elements">
+            <img src="resources.png" alt="" />
+            <span>Resources</span>
+          </div>
+          <div className="route-elements">
+            <img src="contact.png" alt="" />
+            <span>Contact</span>
+          </div>
+        </div>
+        <div className="page-content">
+          dddd
+
+        </div>
+      </main>
     </div>
   );
 };
