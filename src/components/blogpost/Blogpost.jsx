795febc737bdb9e0bd13e0c7ebe5e7d110ec3ac3
Codedys <oberegeraldjackson@gmail.com>
2024-08-27 17:40:27 +0300

    adde a copy button that copies to clipboard for all code blocks

--- src/components/blogpost/Blogpost.jsx
+++ src/components/blogpost/Blogpost.jsx
@@ -1,10 +1,9 @@
-import "./blogpost.css";
-import { useEffect,useState  } from "react";
+// import "./blogpost.css";
+import { useEffect, useState } from "react";
 import { post } from "../../lib/query";
 import { useParams } from "react-router-dom";
 import Prism from "prismjs";
 import "prismjs/themes/prism-tomorrow.css";
-
 
 const Blogpost = () => {
   const { id } = useParams();
@@ -33,17 +32,35 @@ const Blogpost = () => {
     fetchSinglePost(post);
   }, [id]);
 
-
   useEffect(() => {
     if (blog) {
-      Prism.highlightAll(); 
+      Prism.highlightAll();
+
+    
+      const codeBlocks = document.querySelectorAll("pre code");
+      codeBlocks.forEach((block) => {
+        const button = document.createElement("button");
+        button.className = "copy-button";
+        button.innerText = "Copy";
+        block.parentNode.insertBefore(button, block);
+
+        button.addEventListener("click", () => {
+          const code = block.innerText;
+          navigator.clipboard.writeText(code).then(() => {
+            button.innerText = "Copied!";
+            setTimeout(() => {
+              button.innerText = "Copy";
+            }, 2000);
+          });
+        });
+      });
     }
   }, [blog]);
 
   return (
     <div className="blogpost-main">
       {blog ? (
-        <div  dangerouslySetInnerHTML={{ __html: blog }} />
+        <div dangerouslySetInnerHTML={{ __html: blog }} />
       ) : (
         <p>Loading...</p>
       )}
